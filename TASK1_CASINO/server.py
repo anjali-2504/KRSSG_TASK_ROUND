9c3f9edff180ffdff3abeed6f11e186a3d34e1ea
anjali-2504 <75211465+anjali-2504@users.noreply.github.com>
2021-05-23 15:50:04 +0530

    changes made: asked the client for next round and generated new shuffled array of numbers..

--- TASK1_CASINO/server.py
+++ TASK1_CASINO/server.py
@@ -29,84 +29,119 @@ def get_client_num():
 
 
 def gen_arr(r,k):
-    num_arr= [[0 for x in range(r)] for y in range(3*k)]
-    new_arr= [[0 for x in range(r)] for y in range(3*k)] 
+    num_arr= [[0 for x in range(3*k)] for y in range(r)]
+    new_arr= [[0 for x in range(3*k)] for y in range(r)] 
      
     for i in range(r):
         for j in range(3*k):
-            num_arr[i].append(randint(1, 52))
+            num_arr[i][j]=randint(1, 52)
         np.random.shuffle(num_arr[i])
         new_arr[i] = scores(num_arr[i])
     return new_arr
+def initialise(r,k):
+    for y in range(len(score_b)):
+        for x in range(len(score_b[y])):
 
-
+            del score_b[x][y]
+    dict1={}        
+    return (score_b,dict1)      
 def result_board(name,value,r,k):
 
     for i in range(r):
-        print("val ",value[0])
         if(value[0]==i+1):
             score_b[i].append(value[1])
-            print(score_b[i])
-            if(len(score_b[i])==2*k):
+            if(len(score_b[i])==k):
                 high_score=max(score_b[i])
                 winner=list(dict.keys())[list(dict.values()).index((i+1,high_score))]
                 print("Winner of round {} is {}".format(i+1,winner))
                 dict1[winner]+=1
 
-        if(value[0]==r and len(score_b[r-1])==2*k):
+        if(value[0]==r and len(score_b[r-1])==k):
             print("winner of all rounds is  {}".format(max(dict1, key=dict1.get)))
-            
-                    
+names=[] 
+
+
+def shuffle_new(val, a):
+    arr = []
     
-  
-names=[]
+    if(val==1):
+        for i in range(len(a)):
+            for j in range(len(a[i])):
+                arr.append(a[i][j])
+        a_n= [[0 for x in range(len(a[0]))] for y in range(len(a))] 
+        np.random.shuffle(arr)
+        print("shuffled array {}".format(arr))
+        
+        k=0
+        for i in range(len(a)):
+            for j in range(len(a[i])):
+                a_n[i][j]=arr[k]
+                k+=1 
+           
+        return a_n       
+              
+
 def handle_client(c,add,name,score,dict,arrw,client_num,R,k):
     
     connected = True
     name = c.recv(1024).decode(FORMAT)
     names.append(name)
     print("connected with", add,name)
-    dict1[name]=0
+    global dict1
+    
     c.send(bytes("welcome to casino", 'UTF-8'))
     flag=1
     
     while connected:
         play = c.recv(1024).decode(FORMAT)
+        set_of_rounds=0
+        
         if play == DISCONNECT_MESSAGE:
             connected = False
         if play == "YES" or play == "NO":
-           
             if play == "NO":
                 connected = False
             if play == "YES":
+                dict1[name]=0
+                flag=1
+                set_of_rounds+=1
+                print(set_of_rounds)
                 
+                arrw=shuffle_new(client_num,arrw)
+                print(arrw)    
+
                 c.send(bytes("Thanks for playing!!", 'UTF-8'))
                 r=1
                 c.send(pickle.dumps(R))
+                scoretotal=0
                 while(r<R+1):
                 
                     arr=arrw[r-1][3*(client_num-1):3*client_num]
-                    print(arr)
+                   # print(arr)
                     time.sleep(1)
                     c.send(pickle.dumps(arr))
                     time.sleep(1)
                     score=int(pickle.loads((c.recv(1024))))
-                    print("client name ",name,score)
+                    #print("client name ",name,score)
+                    scoretotal+=score
                     dict[name]=(r,score)
                     result_board(name,dict[name],R,k)
                     time.sleep(1)
                     r+=1
                     print(dict)
-                  
-        while(len(score_b[R-1])<2*k):
+        global score_b         
+        while(connected==True and len(score_b[R-1])<k):
             time.sleep(1)         
-        if(len(score_b[R-1])==2*k and flag==1):
-            c.send(pickle.dumps("The winner is{} and you won {} out of {} rounds.".format(max(dict1, key=dict1.get),dict1[name],R)))
+        if(len(score_b[R-1])==k and flag==1):
+            c.send(pickle.dumps("The winner is{} and you won {} out of {} rounds with a score of {}.".format(max(dict1, key=dict1.get),dict1[name],R,scoretotal)))
             flag+=1
         
-
-
-        c.send(bytes("The Game is Over",FORMAT))         
+        time.sleep(5)
+        c.send(bytes("The Game is Over.",FORMAT))
+        print("yes")
+        (score_b,dict1)=initialise(R,k)
+       
+    time.sleep(1)           
     c.close()
    
 
@@ -120,8 +155,6 @@ def scores(num_arr):
             x=13
             new_arr.append(x)   
     return new_arr        
-
-
 
 
 def start(k,r):
@@ -144,5 +177,5 @@ def start(k,r):
 
 print("[STARTING] server is starting...")
 (k,r)=get_client_num()
-score_b= [[0 for x in range(k)] for y in range(r)] 
-start(k,r)  
+score_b= [[]] 
+start(k,r)
